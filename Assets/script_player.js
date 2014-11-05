84205cb1370d73ecc833ac17970a6b66d5d06383
Yaching Chiu <ychiu@kabam.com>
2014-11-04 17:32:37 -0800

    add project

---
+++ Assets/script_player.js
@@ -0,0 +1,35 @@
+﻿//*************************
+//*     Player Script     *
+//*************************
+
+//Inspector Variables
+var speed: float = 8.0;
+var rotationSpeed : float = 100.0;
+//Private Variables
+
+function Start () {
+
+}
+
+function Update () 
+{
+	
+	var transV : float = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+	var transH : float = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+	
+	transform.Translate (transH, transV, 0);
+	
+	if (transform.position.x > 2 )
+		transform.position.x = 2;
+	
+	if (transform.position.x < -2)
+		transform.position.x = -2;
+	
+	
+	if (transform.position.y > 2 )
+		transform.position.y = 2;
+	
+	if (transform.position.y < -2)
+		transform.position.y = -2;
+	
+}
