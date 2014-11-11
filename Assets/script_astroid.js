a72d2d4b0e054bb8bdfd14e9813fa9b9041cf483
Yaching Chiu <ychiu@kabam.com>
2014-11-11 12:56:08 -0800

    Progress - player ui show lives and score

--- Assets/script_astroid.js
+++ Assets/script_astroid.js
@@ -4,6 +4,8 @@ var astroidSpeed : float = 6.0;
 var destroyLimit : float = -9;
 var defaultAstroidY : float = 9;
 
+var explosion	: Transform;
+var sceneManager : GameObject;
 
 function Start () {
 
@@ -23,9 +25,11 @@ function OnTriggerEnter (other : Collider )
 {
 	if( other.gameObject.tag == "Player") {
 		
-		if( other.GetComponent(script_player) != null ) {
-			other.GetComponent(script_player).lives -= 1;
-//			print (other.GetComponent( script_player ).lives);
+		other.GetComponent(script_player).lives -= 1;
+		sceneManager.transform.GetComponent(script_sceneManager).SubstractLife();
+		
+		if( explosion ) {
+			Instantiate (explosion, transform.position, transform.rotation);
 		}
 	}
 }
--- Assets/script_sceneManager.js
+++ Assets/script_sceneManager.js
@@ -3,6 +3,10 @@
 //Inspector Variables
 var gameTime : float = 0.0;
 static var score : int = 0;
+static var lives : int = 3;
+var labelRight : float = 100;
+
+
 //Private Variabls
 
 
@@ -12,10 +16,22 @@ function Start () {
 
 function Update () 
 {
-	print("Player Score: " + score);
+	print("Player Score: " + score + " Player lives: " + lives);
 }
 
 function AddScore()
 {
 	score += 1;
+}
+
+function SubstractLife()
+{
+	lives -= 1;
+}
+
+function OnGUI()
+{
+	GUI.Label(Rect(10, 10, 150, 20), "Player Score: " + score);
+	GUI.Label(Rect(10, 30, 200, 20), "Player Lives: " + lives);
+	GUI.Label(Rect(Screen.width-labelRight, 10, 100, 20), "Counter: " + gameTime);
 }
